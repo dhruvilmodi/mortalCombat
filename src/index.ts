ab00f4f1ef4259f122d0b01029b1de338013a816
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-02-18 18:25:41 -0500

    reverted changes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Arena from './Arena';
 import Kratos from './Kratos';
 
-let dhruvil = new Kratos("Rock and Roll Dhruvil!");
-let jeet = new Kratos("Fabulous JEET!");
-let thisRing = new Arena(dhruvil, jeet);
+let bert = new FighterA("Bert Bertocimus");
+let fred = new FighterA("Fredacious Fredicus");
+let thisRing = new Arena(bert, fred);
